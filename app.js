639b4685c02999dea83ff1dd890423c3ab785b79
ali ashraf <aliashraf@buzzboard.com>
2017-08-14 16:22:46 +0530

    created folder structure to copy bootstrap template. then copied the folders in bootstrap temp to public folder. Then copied index.html to src/views folder. Then added static routes to public and views folder in app.js so that we can access any files present in these folders.

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ var app=express();
 
 var port = 8080;
 
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', function(req, res){
+    res.send('hello world!!!');
+});
+
+app.get('/routing', function(req, res){
+    res.send('hello world to routing!!!');
+});
+
 app.listen(port,function(err){
 console.log('the server is running on port: ' + port);
 });
