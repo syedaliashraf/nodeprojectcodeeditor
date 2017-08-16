9e98fbafd4c60ef8c4615bb46092e36f60629402
ali ashraf <aliashraf@buzzboard.com>
2017-08-16 16:54:28 +0530

    added ejs module so that we can embed javascript inside html files. After installing ejs module, we changed extension of index.html to index.ejs. Then removed src/views from app.use and gave that in app.set. Then added view engine setting using app.set. Then when responding to get resquest, we are redering index.ejs page by passing an array to index.ejs. The name of the variable is list. We are using this array variable in index.ejs and displaying its values.

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ var app=express();
 var port = 8080;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+//app.use(express.static('src/views'));
 app.use(express.static('bower_components'));
 
+app.set('views','./src/views')
+app.set('view engine', 'ejs')
+
+
+
 app.get('/', function(req, res){
-    res.send('hello world!!!');
+    //res.send('hello world!!!');
+    res.render('index',{list:['1st','2nd','3rd']})
 });
 
 app.get('/routing', function(req, res){
