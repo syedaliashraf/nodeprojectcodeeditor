fb88b3e130292d3a8f87e5637f6109083a10a8ad
ali ashraf <aliashraf@buzzboard.com>
2017-08-17 15:14:37 +0530

    Passing events array to events.ejs file. Using that events array to change About us section

--- app.js
+++ app.js
@@ -13,19 +13,56 @@ app.use('/Events', eventRouter);
 app.set('views','./src/views')
 app.set('view engine', 'ejs')
 
+var eventsData = [
+    {
+        name:'event 1',
+        description:'first event',
+        date:'2016.01.01',
+        time:'1.00 pm',
+        duration:'1 hr',
+        location:{
+            streetAddr:'2nd lane',
+            city:'hyd',
+            state:'telangana',
+            zip:'532162',
+            lon:0,
+            lat:0
+
+        },
+        capacity:100
+    },
+    {
+        name:'event 2',
+        description:'second event',
+        date:'2017.01.01',
+        time:'2.00 pm',
+        duration:'2 hr',
+        location:{
+            streetAddr:'3nd lane',
+            city:'hyd',
+            state:'telangana',
+            zip:'532162',
+            lon:0,
+            lat:0
+
+        },
+        capacity:200
+    }
+]
 eventRouter.route('/')
     .get(function(req,res){
     res.render('events',
     {
-        list:['fourth','fifth','sixth'],
-        nav:[
+        list: ['fourth','fifth','sixth'],
+        nav: [
             {Link:'Services', Text:'Services'},
             {Link:'Portfolio', Text:'Portfolio'},
             {Link:'About', Text:'About'},
             {Link:'Team', Text:'Team'},
             {Link:'Contact', Text:'Contact'},
             {Link:'Events', Text:'Events'}
-        ]
+        ],
+        events: eventsData
 }
 );
 })
